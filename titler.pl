8779c3ac7497a85b629eb3f262a365750677c029
gilrim <gilrim@users.noreply.github.com>
2017-01-09 23:40:22 +0100

    well, that'll brake it..

--- titler.pl
+++ titler.pl
@@ -28,34 +28,30 @@ sub get_title {
 
     $_ = $msg;
     if ( $chatnet eq $chan ) {
-        if ( /^http.*.*$/ or /^https.*.*$/ ) {
-            my @splitline = split( ' ', $_ );
-            foreach $word (@splitline) {
-                if ( index( $word, 'http' ) != -1 ) {
-                    $urlfound = 1;
-                    $url      = $word;
-                    last;
-                }
-            }
-            if ( $urlfound eq 1 ) {
-                $p    = HTML::HeadParser->new;
-                $html = (
-`wget -U $useragent --header='Accept-Charset: utf-8' --header='Accept-Language: nb-no, en-us' -q -O- $url`
-                );
-                $p->parse($html);
-                $title = $p->header('Title');
-                $title = substr( $title, 0, 250 );
-                foreach my $d_title (@denied_titles) {
-                    if ( index( $title, $d_title ) ne -1 ) {
-                        return;
-                    }
-                }
-                if ( $title ne '' ) {
-                    $server->command("msg $chan $title");
-                }
-                $urlfound = 0;
-            }
+      $url = ( /https?\:\/\/[\.a-zA-Z0-9\/+:\?\=\&\w_-]{0,500}/i )
+        if $url {
+          $head = (`curl -A $useragent -H 'Accept-Charset: utf-8' -H 'Accept-Language: nb-no, en-us' -I $url`);
+          $content = ( /(Content-Type\: ).*\n /)
+          if $content "#¤&%"#&"¤%&/Y"¤%&"#¤%"
         }
     }
+    if ( $urlfound eq 1 ) {
+        $p    = HTML::HeadParser->new;
+        $html = (`wget -U $useragent --header='Accept-Charset: utf-8' --header='Accept-Language: nb-no, en-us' -q -O- $url`);
+        $p->parse($html);
+        $title = $p->header('Title');
+        $title = substr( $title, 0, 250 );
+        foreach my $d_title (@denied_titles) {
+            if ( index( $title, $d_title ) ne -1 ) {
+                return;
+            }
+        }
+        if ( $title ne '' ) {
+            $server->command("msg $chan $title");
+        }
+        $urlfound = 0;
+    }
+  }
+}
 }
 Irssi::signal_add( 'message public', 'get_title' );
