2d420e2307104ea5100d4d6363b32ecdaec17d56
gilrim <gilrim@users.noreply.github.com>
2017-01-10 03:01:52 +0100

    Update titler.pl
    unbrokenedish

--- titler.pl
+++ titler.pl
@@ -16,6 +16,8 @@ $VERSION = '0.1';
 my $chan = '#channel';
 my $word;
 my $useragent = 'Mozilla';
+my $language = 'Accept-Language: nb-no, en-us';
+my $charset = 'Accept-Charset: utf-8';
 my $url;
 my $urlfound = 0;
 my $title;
@@ -28,27 +30,30 @@ sub get_title {
 
     $_ = $msg;
     if ( $chatnet eq $chan ) {
-      $url = ( /https?\:\/\/[\.a-zA-Z0-9\/+:\?\=\&\w_-]{0,500}/i )
-        if $url {
-          $head = (`curl -A $useragent -H 'Accept-Charset: utf-8' -H 'Accept-Language: nb-no, en-us' -I $url`);
-          $content = ( /(Content-Type\: ).*\n /)
-          if $content "#¤&%"#&"¤%&/Y"¤%&"#¤%"
-        }
-    }
-    if ( $urlfound eq 1 ) {
-        $p    = HTML::HeadParser->new;
-        $html = (`wget -U $useragent --header='Accept-Charset: utf-8' --header='Accept-Language: nb-no, en-us' -q -O- $url`);
-        $p->parse($html);
-        $title = $p->header('Title');
-        $title = substr( $title, 0, 250 );
-        foreach my $d_title (@denied_titles) {
+      ($url) = ($_ =~ /(https?\:\/\/[\.a-zA-Z0-9\/+:\?\=\&\w_-]{0,500})/gi);
+      if ($url) {
+        $head = (`curl -s -A \'$useragent\' -A \'$language\' -A \'$charset\' -I $url`);
+        ($contenttype) = ($head =~ /Content-Type\: (.*)/i);
+        if ($contenttype eq 'text/html'){
+          $p    = HTML::HeadParser->new;
+          $html = (`wget -U $useragent --header=$charset --header=$language -q -O- $url`);
+          $p->parse($html);
+          $title = $p->header('Title');
+          $title = substr( $title, 0, 250 );
+          foreach my $d_title (@denied_titles) {
             if ( index( $title, $d_title ) ne -1 ) {
-                return;
+              return;
             }
+          }
         }
         if ( $title ne '' ) {
             $server->command("msg $chan $title");
         }
+        else
+          {
+            ($size) = ($head =~ /Content-Length\: (.*)/i);
+            $server->command("msg $chan $contenttype:$size");
+          }
         $urlfound = 0;
     }
   }
